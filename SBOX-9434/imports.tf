31916db51035b9ff86d9dfe495c7a36e066ae575
Chilivery <Nagaraj.Chilivery@dhcs.ca.gov>
2024-09-20 14:11:04 -0700

    test

--- SBOX-9434/imports.tf
+++ SBOX-9434/imports.tf
@@ -1,8 +1,8 @@
-# resource "null_resource" "import_baseline_vpc_testvpc" {
-#   provisioner "local-exec" {
-#     command = "terraform import aws_vpc.NIS-SBOX-9434-testvpc-vpc vpc-00582e4a3ec02665b"
-#   }
-# }
+resource "null_resource" "import_baseline_vpc_testvpc" {
+  provisioner "local-exec" {
+    command = "terraform import aws_vpc.NIS-SBOX-9434-testvpc-vpc vpc-00582e4a3ec02665b"
+  }
+}
 # resource "null_resource" "import_customer_ec2" {
 #   provisioner "local-exec" {
 #     command = "terraform import aws_instance.customer_instance i-1234567890abcdef"
