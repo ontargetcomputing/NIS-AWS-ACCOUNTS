1938d67c11a535cdc65cd79e2e7534a9ac67f6d4
Chilivery <Nagaraj.Chilivery@dhcs.ca.gov>
2024-09-22 09:37:02 -0700

    test

--- SBOX-9434/TestFolder/vpc.tf
+++ SBOX-9434/TestFolder/vpc.tf
@@ -1,2 +1,20 @@
-resource aws_vpc "NIS-SBOX-9434-testvpc-vpc" {
+resource "aws_vpc" "NIS-SBOX-9434-testvpc-vpc" {
+  assign_generated_ipv6_cidr_block     = false
+  cidr_block                           = "10.0.0.0/24"
+  enable_dns_hostnames                 = false
+  enable_dns_support                   = true
+  enable_network_address_usage_metrics = false
+  instance_tenancy                     = "default"
+  # ipv4_ipam_pool_id                    = null
+  # ipv4_netmask_length                  = null
+  # ipv6_cidr_block                      = null
+  # ipv6_cidr_block_network_border_group = null
+  # ipv6_ipam_pool_id                    = null
+  ipv6_netmask_length                  = 0
+  tags = {
+    Name = "testvpc"
+  }
+  tags_all = {
+    Name = "testvpc"
+  }
 }
